ab7740aebd01b2b7ca6b53d85df4cf48688d38c0
francescobarbarulo <f.barbarulo@computergross.it>
2023-02-28 10:27:25 +0100

    removed sudo

--- scripts/cni-cilium-install.sh
+++ scripts/cni-cilium-install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+POD_CIDR="192.168.0.0/16"
 CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
-CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-curl -sL --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+CLI_ARCH="amd64"
+
+curl -sSL --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
 rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 
-cilium install --helm-set ipam.operator.clusterPoolIPv4PodCIDRList="192.168.0.0/16"
+cilium install --helm-set ipam.operator.clusterPoolIPv4PodCIDRList=$POD_CIDR
--- scripts/k8s-no-cni-install.sh
+++ scripts/k8s-no-cni-install.sh
@@ -5,74 +5,66 @@ RUNC_VERSION="1.1.4"
 CONTAINERD_VERSION="1.6.16"
 CLI_ARCH="amd64"
 
-if [ ! -z "$1" ]; then
-  K8S_VERSION="$1"
-fi
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
 
 # Installing and configure prerequisites
 
 # Disabling swap
-sudo sed -i 's/\/swap/#\/swap/' /etc/fstab
-sudo swapoff -a
+sed -i 's/\/swap/#\/swap/' /etc/fstab
+swapoff -a
 
 # Add modules permanently
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf > /dev/null
+cat <<EOF | tee /etc/modules-load.d/containerd.conf > /dev/null
 overlay
 br_netfilter
 EOF
 
 # Enable modules
-sudo modprobe overlay
-sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
 # sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf > /dev/null
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf > /dev/null
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
 # Apply kernel params without reboot
-sudo sysctl --system
+sysctl --system
 
 # Installing the container runtime
 
 # Configure containerd
-curl -sLO https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-$CLI_ARCH.tar.gz
-sudo tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-$CLI_ARCH.tar.gz
-rm containerd-$CONTAINERD_VERSION-linux-$CLI_ARCH.tar.gz
-sudo mkdir -p /usr/local/lib/systemd/system
-curl -sL https://raw.githubusercontent.com/containerd/containerd/main/containerd.service | sudo tee /usr/local/lib/systemd/system/containerd.service > /dev/null
+echo "Installing containerd"
+curl -sLO https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-${CLI_ARCH}.tar.gz
+tar Cxzvf /usr/local containerd-${CONTAINERD_VERSION}-linux-${CLI_ARCH}.tar.gz
+rm containerd-${CONTAINERD_VERSION}-linux-${CLI_ARCH}.tar.gz
+mkdir -p /usr/local/lib/systemd/system
+curl -sL https://raw.githubusercontent.com/containerd/containerd/main/containerd.service | tee /usr/local/lib/systemd/system/containerd.service > /dev/null
 
-if [ ! -d /etc/containerd ]; then sudo mkdir /etc/containerd; fi
-containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
-sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+if [ ! -d /etc/containerd ]; then mkdir /etc/containerd; fi
+containerd config default | tee /etc/containerd/config.toml > /dev/null
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
-sudo systemctl daemon-reload
-sudo systemctl enable --now containerd
+systemctl daemon-reload
+systemctl enable --now containerd
 
 # Install runc
-curl -sLO https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.$CLI_ARCH
-sudo install -m 755 runc.$CLI_ARCH /usr/local/bin/runc
-rm runc.$CLI_ARCH
-
-# Configure crictl
-# cat <<EOF | sudo tee /etc/crictl.yaml > /dev/null
-# runtime-endpoint: unix:///run/containerd/containerd.sock
-# image-endpoint: unix:///run/containerd/containerd.sock
-# timeout: 10
-# EOF
+echo "Installing runc"
+curl -sLO https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.${CLI_ARCH}
+install -m 755 runc.${CLI_ARCH} /usr/local/bin/runc
+rm runc.${CLI_ARCH}
 
 
 # Installing kubeadm, kubelet and kubectl
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet=$K8S_VERSION-00 kubeadm=$K8S_VERSION-00 kubectl=$K8S_VERSION-00
-sudo apt-mark hold kubelet kubeadm kubectl
+echo "Installing kubeadm, kubelet and kubectl"
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet=$K8S_VERSION-00 kubeadm=$K8S_VERSION-00 kubectl=$K8S_VERSION-00
+apt-mark hold kubelet kubeadm kubectl
 
 
 # Init control-plane
@@ -95,15 +87,4 @@ kind: KubeletConfiguration
 apiVersion: kubelet.config.k8s.io/v1beta1
 cgroupDriver: "systemd"
 EOF
-
-# Init the control plane node
-sudo kubeadm init --config ~/kubeadm-config.yaml
-
-# Copy the admin config file to the default kubectl directory
-if [ ! -d ~/.kube ]; then mkdir ~/.kube; fi
-# sudo cp /etc/kubernetes/admin.conf ~/.kube/config
-# sudo chown $USER:$USER ~/.kube/config
-
-# kubectl auto-completion
-kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
-source ~/.bashrc
+kubeadm init --config ~/kubeadm-config.yaml

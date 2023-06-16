8c431ed0e22839efa33deb7c26f269f5702c477e
francescobarbarulo <f.barbarulo@computergross.it>
2023-06-16 15:03:15 +0200

    swap file content

--- scripts/lxd/kubeadm-init.sh
+++ scripts/lxd/kubeadm-init.sh
@@ -1,68 +1,27 @@
 #!/bin/sh
 
-RUNC_VERSION="1.1.4"
-CONTAINERD_VERSION="1.6.16"
-CRICTL_VERSION="1.26.0"
 K8S_VERSION="1.25.6"
 
-case $(uname -m) in
-  "x86_64") ARCH="amd64" ;;
-  "aarch64") ARCH="arm64"   ;;
-  *) echo "Architecture not supported"; exit ;;
-esac
-
-echo "arch:        $ARCH"
-echo "runc:        $RUNC_VERSION"
-echo "containerd:  $CONTAINERD_VERSION"
-echo "crictl:      $CRICTL_VERSION"
-echo "kubernetes:  $K8S_VERSION"
-
-# Configure containerd
-echo "Installing containerd"
-curl -sSLO https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-${ARCH}.tar.gz
-tar Cxzf /usr/local containerd-${CONTAINERD_VERSION}-linux-${ARCH}.tar.gz
-rm -f containerd-${CONTAINERD_VERSION}-linux-${ARCH}.tar.gz
-mkdir -p /usr/local/lib/systemd/system
-curl -sL https://raw.githubusercontent.com/containerd/containerd/main/containerd.service | tee /usr/local/lib/systemd/system/containerd.service > /dev/null
-
-if [ ! -d /etc/containerd ]; then mkdir /etc/containerd; fi
-containerd config default | tee /etc/containerd/config.toml > /dev/null
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-
-systemctl daemon-reload
-systemctl enable --now containerd
-
-# Install runc
-echo "Installing runc"
-curl -sLO https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.${ARCH}
-install -m 755 runc.${ARCH} /usr/local/bin/runc
-rm -f runc.${ARCH}
-
-# Install and configure crictl
-echo "Installing crictl"
-curl -sLO https://github.com/kubernetes-sigs/cri-tools/releases/download/v${CRICTL_VERSION}/crictl-v${CRICTL_VERSION}-linux-${ARCH}.tar.gz
-tar zxf crictl-v${CRICTL_VERSION}-linux-${ARCH}.tar.gz -C /usr/local/bin
-rm -f crictl-v${CRICTL_VERSION}-linux-${ARCH}.tar.gz
-cat <<EOF | tee /etc/crictl.yaml > /dev/null
-runtime-endpoint: unix:///run/containerd/containerd.sock
-image-endpoint: unix:///run/containerd/containerd.sock
-timeout: 2
+# Init control-plane
+cat <<EOF | tee ~/kubeadm-config.yaml > /dev/null
+kind: InitConfiguration
+apiVersion: kubeadm.k8s.io/v1beta3
+nodeRegistration:
+  name: "cp-01"
+  criSocket: "unix:///run/containerd/containerd.sock"
+---
+kind: ClusterConfiguration
+apiVersion: kubeadm.k8s.io/v1beta3
+kubernetesVersion: v$K8S_VERSION
+clusterName: "kubernetes"
+networking:
+  dnsDomain: "cluster.local"
+  serviceSubnet: "10.96.0.0/12"
+---
+kind: KubeletConfiguration
+apiVersion: kubelet.config.k8s.io/v1beta1
+cgroupDriver: "systemd"
 EOF
 
-# Installing kubeadm
-echo "Installing kubeadm"
-curl -sLO https://dl.k8s.io/release/v${K8S_VERSION}/bin/linux/${ARCH}/kubeadm
-install kubeadm /usr/local/bin/kubeadm
-rm -f kubeadm
-
-# Installing kubelet
-# NOTE: kubeadm does not create /etc/kubernetes/manifests directory. Make dir by hand
-echo "Installing kubelet"
-curl -sLO https://dl.k8s.io/release/v${K8S_VERSION}/bin/linux/${ARCH}/kubelet
-install kubelet /usr/local/bin/kubelet
-rm -f kubelet
-RELEASE_VERSION="$(curl -s https://api.github.com/repos/kubernetes/release/releases/latest | grep tag_name | sed -E 's/.*"([^"]+)".*/\1/')"
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:/usr/local/bin:g" | tee /usr/local/lib/systemd/system/kubelet.service > /dev/null
-mkdir -p /usr/local/lib/systemd/system/kubelet.service.d
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:/usr/local/bin:g" | tee /usr/local/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
-systemctl enable --now kubelet
+echo "Init Control Plane"
+kubeadm init --config ~/kubeadm-config.yaml --ignore-preflight-errors=all

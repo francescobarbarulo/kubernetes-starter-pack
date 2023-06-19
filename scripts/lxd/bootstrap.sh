4e64fc95e14f0afccf08611e37ba06ae84984d9c
francescobarbarulo <f.barbarulo@computergross.it>
2023-06-19 14:34:28 +0200

    add bootsrtap script and edit k8s profile

---
+++ scripts/lxd/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Init lxd
+cat ~/lab/lxd-config | lxd init --preseed
+
+# Create k8s profile
+lxc profile create k8s && cat ~/lab/lab-profile-config | lxc profile edit k8s
+
+# Create lab instances
+INSTANCES="docker k8s-cp-01 k8s-w-01"
+
+for instance in $INSTANCES
+
+do
+  lxc init images:ubuntu/jammy/cloud $instance --profile k8s
+  lxc start $instance
+done

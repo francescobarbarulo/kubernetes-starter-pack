87eda5bf1ae7741ac705df1a0345c9da5e155d2e
francescobarbarulo <f.barbarulo@computergross.it>
2023-06-22 17:12:30 +0200

    install linux image kernel

--- scripts/lxd/bootstrap.sh
+++ scripts/lxd/bootstrap.sh
@@ -8,11 +8,12 @@ lxc profile create k8s
 cat ./k8s-profile-config | lxc profile edit k8s
 
 # Create lab instances
-INSTANCES="docker k8s-cp-01 k8s-w-01"
+INSTANCES="dev registry k8s-cp-01 k8s-w-01"
 
 for instance in $INSTANCES
 
 do
   lxc init images:ubuntu/jammy/cloud $instance --profile k8s
   lxc start $instance
+  lxc exec $instance -- apt install -y linux-image-$(uname -r)
 done

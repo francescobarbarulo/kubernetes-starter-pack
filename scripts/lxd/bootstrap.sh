6248c09365b6189c53adcb9146ef0652eefdfa68
francescobarbarulo <f.barbarulo@computergross.it>
2023-06-23 16:40:35 +0200

    fixed lab bootstrap

--- scripts/lxd/bootstrap.sh
+++ scripts/lxd/bootstrap.sh
@@ -4,8 +4,7 @@
 cat ./lxd-config | lxd init --preseed
 
 # Create k8s profile
-lxc profile create k8s
-cat ./k8s-profile-config | lxc profile edit k8s
+lxc profile list | grep -qo k8s || (lxc profile create k8s && cat ./k8s-profile-config | lxc profile edit k8s)
 
 # Create lab instances
 INSTANCES="dev registry k8s-cp-01 k8s-w-01"
@@ -15,5 +14,11 @@ for instance in $INSTANCES
 do
   lxc init images:ubuntu/jammy/cloud $instance --profile k8s
   lxc start $instance
-  lxc exec $instance -- apt install -y linux-image-$(uname -r)
+done
+
+for instance in $INSTANCES
+
+do
+  lxc exec $instance -- cloud-init status --wait
+  lxc exec $instance -- apt-get install -y linux-image-$(uname -r)
 done

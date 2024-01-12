9870318585d1bc8a22a3bc6c8d944b2819e8ff54
francescobarbarulo <f.barbarulo@computergross.it>
2024-01-12 11:46:33 +0100

    create base image

--- scripts/lab/bootstrap.sh
+++ scripts/lab/bootstrap.sh
@@ -11,8 +11,24 @@ sysctl -p /etc/sysctl.d/99-ksp.conf > /dev/null
 # Init Incus
 curl -sL $REPO/$PREFIX/incus-config | incus admin init --preseed
 
+# Create base profile
+incus profile list | grep -qo base || incus profile create base
+curl -sL $REPO/$PREFIX/incus-base-profile | incus profile edit base
+
+incus init images:ubuntu/jammy/cloud default --profile base
+incus start default
+
+# Wait for init
+incus exec default -- cloud-init status --wait
+incus stop default
+
+# Create base image
+incus publish default local: --alias base
+incus delete default
+
 # Create k8s profile
-incus profile list | grep -qo k8s || (incus profile create k8s && curl -sL $REPO/$PREFIX/k8s-profile-config | incus profile edit k8s)
+incus profile list | grep -qo k8s || incus profile create k8s
+curl -sL $REPO/$PREFIX/incus-k8s-profile | incus profile edit k8s
 
 # Create lab instances
 INSTANCES="dev:172.30.10.10 registry:172.30.10.11 nfs:172.30.10.12 k8s-cp-01:172.30.10.20 k8s-w-01:172.30.10.21 k8s-w-02:172.30.10.22"
@@ -22,6 +38,6 @@ for instance in $INSTANCES
 do
   name=$(echo $instance | cut -d ":" -f 1)
   ipv4_addr=$(echo $instance | cut -d ":" -f 2)
-  incus init images:ubuntu/jammy/cloud $name -d eth0,ipv4.address=$ipv4_addr --profile k8s
+  incus init local:base $name -d eth0,ipv4.address=$ipv4_addr --profile k8s
   incus start $name
 done
